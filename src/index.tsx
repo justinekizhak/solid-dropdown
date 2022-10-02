31658ca661a704f213ee66973839ba479fffa33b
Justine Kizhakkinedath <justine@kizhak.com>
2022-10-02 17:27:18 +0530

    feat: Add render function example

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,19 @@ import App from "./App";
 import { DropdownProp } from "./DropdownTypes";
 
 const dropdownProps: DropdownProp = {
-  button: "Menu",
+  button: {
+    key: "menu-key",
+    render: () => {
+      return (
+        <div class="flex items-center">
+          <span>Menu JSX</span>
+          <svg width="20" height="20" class="ml-2 inline-block">
+            <circle cy="10" cx="10" r="5" class="fill-blue-500" />
+          </svg>
+        </div>
+      );
+    },
+  },
   options: ["option 1", "option 2", "option 3"],
   onMenuClick: (isMenuOpen) => console.log({ isMenuOpen }),
   onOptionClick: (optionKey) => console.log({ optionKey }),
